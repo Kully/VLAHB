d1fad69af10634ed7e7dc581ecfd7613a86b70df
Kully <adam@plot.ly>
2019-04-14 16:21:37 -0400

    start logic for op commands and vm reading a file

--- vm.py
+++ vm.py
@@ -1,17 +1,87 @@
-# read hex file
-f = open('file.hex', 'r')
-lines_from_hex_file = f.readlines()
-bin_file_string = ''
+'''
+Virtual Machine
+
+The vm is what would be the PCB, etched silicon 
+
+The file.hex that the vm looks at is of the form
+
+```
+hhhhhhhh
+hhhhhhhh
+
+hhhhhhhh
+hhhhhhhh
+
+...
+```
+
+where `h` is a hexadecimal value which takes on a value
+between 0-f (16 unique values).
+
+This is how 2 rows (32x2 bits) is interpreted
+
+```
+[index in RAM][command]
+[value]
+```
+
+PC = program counter
+'''
+import pprint
+
+ROM = []
+RAM = [None] * 2**16
+PC = []
+ALU = ['+', '-', '*', '%']
+
+def return_lines_from_file_hex(file_hex):
+	f = open(file_hex, 'r')
+	lines = f.read().split('\n')
+	while '' in lines:
+		lines.remove('')
+	return lines
+
+def hex_to_int(h):
+	return int(h, 16)
+
+def read_and_exec_hex_file(file_hex):
+	lines_from_file_hex = return_lines_from_file_hex(file_hex)
+
+	isCommand = True
+	for PC, line in enumerate(lines_from_file_hex):
+		print('PC: %r' %PC)
+		print('line: %r\n' %line)
+		if isCommand:
+			index_in_register = hex_to_int(line[:4])
+			command = line[4:]
+
+		else:
+			value = line
+			if command == '0001':  # LD
+				RAM[index_in_register] = hex_to_int(value)
+
+			elif command == '0002':  # ADD
+				if RAM[index_in_register] is None:
+					RAM[index_in_register] = 0
+				RAM[index_in_register] += hex_to_int(value)
+
+			elif command == '0003':  # SUB
+				if RAM[index_in_register] is None:
+					RAM[index_in_register] = 0
+				RAM[index_in_register] -= hex_to_int(value)
+
+			elif command == '0004':  # GOTO
+				pass
+		isCommand = not isCommand
 
 
-def hex_string_to_bin(x):
-    return bin(int(x, 16))[2:]
+read_and_exec_hex_file('file.hex')
+pprint.pprint(RAM[:11])
 
-is_op_code = True
-for line in lines_from_hex_file:
-    if line != '\n':
-        bin_file_string += hex_string_to_bin(line)
 
-print(bin_file_string)
 
-# write bin file
+
+
+
+
+
