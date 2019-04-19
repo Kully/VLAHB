a6c5e1f9a754853b95880172b1312a5b6744aa9f
Kully <adam@plot.ly>
2019-04-18 22:27:46 -0400

    place print statements after command is done

--- vm.py
+++ vm.py
@@ -8,10 +8,10 @@ For now, vm.py looks at the hex file (file.hex)
 Eventually, vm.py will look at the binary (file.bin)
 
 ```
-hhhhhhhh
-hhhhhhhh
-hhhhhhhh
-hhhhhhhh
+hhhh hhhh
+hhhh hhhh
+hhhh hhhh
+hhhh hhhh
 ...
 ```
 
@@ -25,6 +25,10 @@ hhhh[index in RAM]hhhh[command]
 hhhhhhhh[value]
 ```
 
+A WORD is a 4byte string of hex values, or 32 bits
+```
+hhhh hhhh
+```
 
 An example of my setup:
 ```python
@@ -50,7 +54,7 @@ COMMANDS_PER_SEC = 10
 DELAY_BETWEEN_COMMANDS = 1. / COMMANDS_PER_SEC  # in seconds
 ROM = []
 # TODO: make RAM dynamic - index as I go
-RAM = [None] * 2**16
+RAM = ['-'] * 2**16
 RAM_SIZE = 128000  # units of 4 bytes // 512KB
 MAX_RAM_NUMBER = 2**32  # largest value in a slot of RAM (hhhhhhhh)
 
@@ -81,13 +85,13 @@ def validate_hex_file(file_hex, remove_empty_lines=True):
     lines = return_lines_from_file_hex(file_hex)
 
     assert len(lines) % 2 == 0, util.EVEN_NUMBER_HEX_LINES_ERROR_MSG
-    print('    even number of hex lines (ok)')
+    print('    - even number of hex lines (ok)')
 
     assert all(len(line) == 8 for line in lines), util.CHARS_PER_LINE_ERROR_MSG
-    print('    all lines in file.hex are 8 chars long (ok)')
+    print('    - all lines in file.hex are 8 chars long (ok)')
 
     assert all(char in string.hexdigits + 'x' for char in "".join(lines)), util.VALID_HEX_VALUES_ERROR_MSG
-    print('    all chars are valid hexadecimal (ok)')
+    print('    - all chars are valid hexadecimal (ok)')
 
     print('Validation: PASS!\n')
 
@@ -104,65 +108,64 @@ def exec(lines_from_file_hex):
             break
 
         print('\nPC: %r'%PC)
-        print('    next 2 lines in ROM:')
+        print('    ROM lines:')
         print('        %s'%ROM[PC])
         print('        %s'%ROM[PC+1])
 
-        # convert all hex to int
         GOTO = False
-        index_in_RAM = util.hex_to_int(ROM[PC][:4])
-        command = util.hex_to_int(ROM[PC][4:])
-        value = util.hex_to_int(ROM[PC+1])
 
-        print('    RAM[0]: %s'%RAM[0])
+        # convert all hex to int
+        word0_first_half = util.hex_to_int(ROM[PC][:4])
+        word0_second_half = util.hex_to_int(ROM[PC][4:])
+        word1 = util.hex_to_int(ROM[PC+1])
+
+        if word0_second_half == 1:  # LD (load)
+            RAM[word0_first_half] = word1
+            print('\n    LD {} to RAM[{}]'.format(
+                word1, word0_first_half
+            ))
+
+        elif word0_second_half == 2:  # ADD
+            if RAM[word0_first_half] is '-':
+                RAM[word0_first_half] = 0
+            RAM[word0_first_half] += word1
+            print('    ADD {} to RAM[{}]'.format(
+                word1, word0_first_half
+            ))
+            manage_stack_over_under_flow(word0_first_half)
+
+        elif word0_second_half == 3:  # SUB (subtract)
+            if RAM[word0_first_half] is '-':
+                RAM[word0_first_half] = 0
+            RAM[word0_first_half] -= word1
+            print('    SUB {} from RAM[{}]={}'.format(
+                word1, word0_first_half, RAM[word0_first_half]
+            ))
+            manage_stack_over_under_flow(word0_first_half)
+
+        elif word0_second_half == 4:  # GOTO (go to)
+            GOTO = True
+
+        elif word0_second_half == 5:  # ALD (address load)
+            RAM[PC] = RAM[word1]
+            print('    LD RAM[%s] to RAM[%s]' %())
+
+        # increment program counter
+        if GOTO:
+            PC = word1
+            print('    GOTO line %s' %word1)
+        else:
+            PC += 2
+
+        print('\n    RAM[0]: %s'%RAM[0])
         print('    RAM[1]: %s'%RAM[1])
         print('    RAM[2]: %s'%RAM[2])
         print('    RAM[3]: %s'%RAM[3])
         print('    RAM[4]: %s'%RAM[4])
 
-        if command == 1:  # LD (load)
-            RAM[index_in_RAM] = value
-            print('\n    LD {} to RAM[{}]'.format(
-                value, index_in_RAM
-            ))
-
-        elif command == 2:  # ADD
-            if RAM[index_in_RAM] is None:
-                RAM[index_in_RAM] = 0
-            RAM[index_in_RAM] += value
-            print('    ADD {} to RAM[{}]'.format(
-                value, index_in_RAM
-            ))
-            manage_stack_over_under_flow(index_in_RAM)
-
-        elif command == 3:  # SUB (subtract)
-            if RAM[index_in_RAM] is None:
-                RAM[index_in_RAM] = 0
-            RAM[index_in_RAM] -= value
-            print('    SUB {} from RAM[{}]={}'.format(
-                value, index_in_RAM, RAM[index_in_RAM]
-            ))
-            manage_stack_over_under_flow(index_in_RAM)
-
-        elif command == 4:  # GOTO (go to)
-            GOTO = True
-
-        elif command == 5:  # ALD (address load)
-            RAM[PC] = RAM[value]
-
-        # increment program counter
-        if GOTO:
-            PC = value
-            print('    GOTO line %s' %value)
-        else:
-            PC += 2
-
-
 myHexFileName = 'file.hex'
 hex_lines = return_lines_from_file_hex(myHexFileName)
 fill_ROM_with_hex_lines(hex_lines)
 validate_hex_file(myHexFileName)
+time.sleep(1)
 exec(hex_lines)
-
-print('\nRAM after program is done:')
-print('    %s'%RAM[:10])
