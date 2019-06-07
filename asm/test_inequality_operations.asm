ef645a08d0c972f660ef055bb1a7a791476b43b9
Kully <adam@plot.ly>
2019-06-07 17:09:45 -0400

    make a test for inequality opcodes in asm/

--- asm/test_inequality_operations.asm
+++ asm/test_inequality_operations.asm
@@ -1,35 +1,50 @@
-// The idea is that each time one of these tests fails
-// R[0] goes up by 1.
+// tests for <, <=, >, >=
+//           LT LTE GT GTE
 //
-// Therefore we get a count of the total number of
-// failures of the tests
-//
-//
+// R[0] <- number of test failures
+// R[1] <- number of tests
 
-// eventually move these out of the 0-16 RAM index range
+// TODO - eventually move these out of the 0-16 RAM index range
 
 LD R[0] 0  // number of test failures
-LD R[1] 2  // number of tests
+LD R[1] 12  // number of tests
 
-// load values
+// load values in RAM
 LD R[8004] 4
 LD R[8005] 5
 LD R[8006] 6
 
-//
-// TESTS
-//
-
-// less than <
-
+// test: <
 LT R[8005] 6
-ADD R[0] 1
+	ADD R[0] 1
 LT R[8005] R[8006]
-ADD R[0] 1
+	ADD R[0] 1
 
-// test <=
-// test >
-// test >=
+// test: <=
+LTE R[8005] 6
+	ADD R[0] 1
+LTE R[8005] R[8006]
+	ADD R[0] 1
+LTE R[8006] 6
+	ADD R[0] 1
+LTE R[8006] R[8006]
+	ADD R[0] 1
+
+// test: >
+GT R[8006] 5
+	ADD R[0] 1
+GT R[8006] R[8005]
+	ADD R[0] 1
+
+// test: >=
+GTE R[8006] 5
+	ADD R[0] 1
+GTE R[8006] R[8005]
+	ADD R[0] 1
+GTE R[8006] 6
+	ADD R[0] 1
+GTE R[8006] R[8006]
+	ADD R[0] 1
 
 EXIT
 
