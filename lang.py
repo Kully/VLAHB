4f49d9c46b55d5ebb338fcb8e559b4970afbd0ee
Gustav Louw <airglouw@gmail.com>
2019-07-11 13:41:12 -0700

    add(scopes + idents + assign)

--- lang.py
+++ lang.py
@@ -3,24 +3,27 @@ class Compiler:
     def __init__(self, lang, asm):
         self.lang = open(lang, 'r')
         self.asm = open(asm, 'w')
-        self.char = None
-        self.labels = []
+        self.look = None
         self.sp = 0
+        self.idents = {}
 
     def __del__(self):
         self.lang.close()
         self.asm.close()
 
+    def bomb(self, message):
+        print("error: " + message)
+        exit(1)
+
     def feed(self):
         while True:
-            self.char = self.lang.read(1)
-            if not self.char.isspace():
+            self.look = self.lang.read(1)
+            if not self.look.isspace():
                 break
 
     def match(self, expected):
-        if self.char != expected:
-            print("error: expected %s but got %s" % (expected, self.char))
-            exit(1)
+        if self.look != expected:
+            self.bomb("expected '%s' but got %s" % (expected, self.look))
         self.feed()
 
     def isdigit(self, value):
@@ -29,102 +32,144 @@ class Compiler:
     def digit(self):
         chars = []
         while True:
-            chars.append(self.char)
+            chars.append(self.look)
             self.feed()
-            if not self.isdigit(self.char):
+            if not self.isdigit(self.look):
                 break
         digit = ''.join(chars)
-        self.asm.write("\tLD R[%d] %s\n" % (self.sp, digit))
-        self.sp += 1
+        return digit
+
+    def call(self, ident):
+        self.asm.write("\tCALL %s\n" % ident)
+        self.asm.write("\tLD R{%d] R[4100]\n" % self.sp)
+
+    def ident(self):
+        ident = self.string()
+        if self.idents.get(ident) is None:
+            self.bomb("'%s' undefined" % ident)
+        if self.look == '(':
+            self.match('(')
+            # ... args.
+            self.match(')')
+            self.call(ident)
+        else:
+            self.asm.write("\tLD R[%d] R[%d]\n" % (self.sp, self.idents[ident]))
 
     def factor(self):
-        if self.char == '(':
+        if self.look == '(':
             self.match('(')
             self.expression()
             self.match(')')
+        elif self.look.isalpha():
+            self.ident()
         else:
-            self.digit()
+            digit = self.digit()
+            self.asm.write("\tLD R[%d] %s\n" % (self.sp, digit))
+        self.sp += 1
 
     def mul(self):
         self.match('*')
         self.factor()
         self.asm.write("\tMUL R[%d] R[%d]\n" % (self.sp - 2, self.sp - 1))
-        self.sp -= 1
 
     def div(self):
         self.match('/')
         self.factor()
         self.asm.write("\tDIV R[%d] R[%d]\n" % (self.sp - 2, self.sp - 1))
-        self.sp -= 1
 
     def term(self):
         self.factor()
-        while self.char in ['*', '/']:
-            if self.char == '*':
-                self.mul()
-            if self.char == '/':
-                self.div()
+        while self.look in ['*', '/']:
+            operate = {
+                '*': self.mul,
+                '/': self.div,
+            }
+            operate[self.look]()
+            self.sp -= 1
 
     def add(self):
         self.match('+')
         self.term()
         self.asm.write("\tADD R[%d] R[%d]\n" % (self.sp - 2, self.sp - 1))
-        self.sp -= 1
 
     def sub(self):
         self.match('-')
         self.term()
         self.asm.write("\tSUB R[%d] R[%d]\n" % (self.sp - 2, self.sp - 1))
-        self.sp -= 1
 
     def expression(self):
         self.term()
-        while self.char in ['+', '-']:
-            if self.char == '+':
-                self.add()
-            if self.char == '-':
-                self.sub()
+        while self.look in ['+', '-']:
+            operate = {
+                '+': self.add,
+                '-': self.sub,
+            }
+            operate[self.look]()
+            self.sp -= 1
 
-    def islabel(self, ch):
-        return self.char.isalnum() or ch == '_'
+    def isstring(self, ch):
+        return self.look.isalnum() or ch == '_'
 
-    def label(self):
+    def string(self):
         chars = []
         while True:
-            chars.append(self.char)
+            chars.append(self.look)
             self.feed()
-            if not self.islabel(self.char):
+            if not self.isstring(self.look):
                 break
-        label = ''.join(chars)
-        self.labels.append(label)
-        self.asm.write("%s:\n" % label)
+        string = ''.join(chars)
+        return string
+
+    def label(self):
+        label = self.string()
+        if self.idents.get(label) is None:
+            self.idents[label] = -1
+            self.asm.write("%s:\n" % label)
+        else:
+            self.bomb("label '%s' already defined" % label)
 
     def args(self):
         self.match('(')
-        if self.char != ')':
+        if self.look != ')':
             while True:
                 self.expression()
-                if self.char == ',':
+                if self.look == ',':
                     self.match(',')
                 else:
                     break
         self.match(')')
 
+    def assign(self, string):
+        self.match('=')
+        if self.idents.get(string) is None:
+            self.idents[string] = self.sp
+        else:
+            self.bomb("%s already defined" % string)
+
     def block(self):
+        strings = []
         self.match('{')
-        if self.char != '}':
+        if self.look != '}':
             while True:
+                string = self.string()
+                strings.append(string)
+                self.assign(string)
                 self.expression()
                 self.match(';')
-                if self.char == '}':
+                if self.look == '{':
+                    self.block()
+                if self.look == '}':
                     break
         self.match('}')
+        for string in strings:
+            del self.idents[string]
+        self.sp -= len(strings)
 
     def function(self):
         self.sp = 0
         self.args()
         self.block()
-        self.asm.write("\tLD R[4100] R[%d]\n" % (self.sp - 1))
+        self.asm.write("\tLD R[4100] R[%d]\n" % self.sp)
         self.asm.write("\tRETURN\n\n")
 
     def ishex(self, value):
@@ -133,9 +178,9 @@ class Compiler:
     def word(self):
         chars = []
         while True:
-            chars.append(self.char)
+            chars.append(self.look)
             self.feed()
-            if not self.ishex(self.char):
+            if not self.ishex(self.look):
                 break
         word = ''.join(chars)
         self.asm.write("\t0x%s\n" % word)
@@ -144,7 +189,7 @@ class Compiler:
         delim = ','
         while True:
             self.word()
-            if self.char == delim:
+            if self.look == delim:
                 self.match(delim)
             else:
                 break
@@ -163,14 +208,12 @@ class Compiler:
     def compile(self):
         self.reset()
         self.feed()
-        while self.char:
+        while self.look:
             self.label()
-            if self.char == '[':
+            if self.look == '[':
                 self.array()
-            if self.char == '(':
+            if self.look == '(':
                 self.function()
-        for label in self.labels:
-            print(label)
 
 if __name__ == "__main__":
     Compiler('lang/test.lang', 'asm/test.asm').compile()
