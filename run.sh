9dcfb64a5d83f9f19d6adc7c62c04666fd691db7
Kully <adam@plot.ly>
2019-08-11 11:56:05 -0700

    spacing in run.sh line

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ filename="pride_flag"
 # -O turns printing off
 python3 asm.py $filename.asm
 gcc -Og -g -fsanitize=address bin.c && ./a.out hex/file.hex bin/file.bin
-gcc -Og -g -fsanitize=address vm.c  -lSDL2 -lm  && ./a.out bin/file.bin
+gcc -Og -g -fsanitize=address vm.c -lSDL2 -lm && ./a.out bin/file.bin
