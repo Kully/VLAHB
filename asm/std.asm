30fe900421bfa559af5f864fa504d867d6086b59
Kully <adam@plot.ly>
2019-09-07 13:54:48 -0400

    cleanup(std file//tests asm folder)

--- asm/std.asm
+++ asm/std.asm
@@ -183,3 +183,163 @@ FILL_VRAM_WITH_RAND_VALUES:
 	GTE R[4096] 23301
 	GOTO FILL_VRAM_WITH_RAND_VALUES
 	RETURN
+
+
+// Here I use RAM slot 512 as the return register.
+// We should agree on a proper return register.
+
+// var MATH_ADD(var a, var b)
+// {
+//     a += b
+//     return a
+// }
+MATH_ADD:
+    ADD R[0] R[1]
+    LD R[4100] R[0]
+    RETURN
+
+// var MATH_SUB(var a, var b)
+// {
+//     a -= b
+//     return a
+// }
+MATH_SUB:
+    SUB R[0] R[1]
+    LD R[4100] R[0]
+    RETURN
+
+// var MATH_MUL(var a, var b)
+// {
+//     a *= b
+//     return a
+// }
+MATH_MUL:
+    MUL R[0] R[1]
+    LD R[4100] R[0]
+    RETURN
+
+// var MATH_DIV(var a, var b)
+// {
+//     a /= b
+//     return a
+// }
+MATH_DIV:
+    DIV R[0] R[1]
+    LD R[4100] R[0]
+    RETURN
+
+// var MATH_LONG_WINDED(var a, var b)
+// {
+//     b = MATH_ADD(a, b)
+//     b = MATH_ADD(a, b)
+//     b = MATH_ADD(a, b)
+//     b = MATH_ADD(a, b)
+//     return b//
+// }
+MATH_LONG_WINDED:
+    CALL MATH_ADD
+    LD R[1] R[4100] // 15
+    CALL MATH_ADD
+    LD R[1] R[4100] // 25
+    CALL MATH_ADD
+    LD R[1] R[4100] // 35
+    CALL MATH_ADD
+    RETURN // 45
+
+
+// TODO: what should happen if you divide by 0?
+
+// Python: R[0] // R[1]
+MATH_FLOOR_DIV:
+    GTE R[0] R[1]
+    RETURN
+    ADD R[4100] 1
+    SUB R[0] R[1]
+    GOTO MATH_FLOOR_DIV
+
+// Python: R[0] mod R[1]
+MATH_DIV_REMAINDER:
+    CALL MATH_FLOOR_DIV
+    LD R[3] R[1]
+    MUL R[3] R[4100]
+    
+    LD R[4] R[0]
+    SUB R[4] R[3]  // stack underflow
+    
+    LD R[4100] R[4]
+    RETURN
+
+// Python: R[0] // 16
+MATH_FLOOR_DIV_16:
+    GTE R[0] 16
+        RETURN
+    ADD R[4100] 1
+    SUB R[0] 16
+    GOTO MATH_FLOOR_DIV_16
+
+// Python: R[0] % 16
+MATH_DIV_REMAINDER_16:
+    CALL MATH_FLOOR_DIV_16
+    LD R[3] 16
+    MUL R[3] R[4100]
+    
+    LD R[4] R[0]
+    SUB R[4] R[3]  // stack underflow
+    
+    LD R[4100] R[4]
+    RETURN
+
+// R[0]: X
+// R[1]: Y
+// Returns abs(X-Y)
+MATH_ABS_DIFF:
+    LTE R[0] R[1]
+    GOTO MATH_ABS_DIFF_LTE_FALSE
+    LD R[4100] R[1]
+    SUB R[4100] R[0]
+    RETURN
+
+    MATH_ABS_DIFF_LTE_FALSE:
+        LD R[4100] R[0]
+        SUB R[4100] R[1]
+        RETURN
+
+    RETURN
+
+
+
+
+
+
+LOGIC_OR:
+	LD R[4100] 1
+	CMP R[0] 0
+	RETURN
+	CMP R[1] 0
+	RETURN
+	LD R[4100] 0
+	RETURN
+
+
+LOGIC_AND:
+ 	// count num of 1s in R[4]
+	LD R[4100] 0
+
+	CMP R[0] 0
+	ADD R[4] 1
+
+	CMP R[1] 0
+	ADD R[4] 1
+
+	CMP R[4] 2
+	RETURN
+	LD R[4100] 1
+	LD R[4] 0  // reset R[4] to 0
+	RETURN
+
+LOGIC_NOT:
+	CMP R[0] 0
+	LD R[4100] 0
+	LD R[4100] 1
+	RETURN
+
