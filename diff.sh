4eff2b7284a26841c908cdbd0c296c4e77099bee
Kully <adam@plot.ly>
2019-09-09 14:21:53 -0400

    CLEANUP: remove old vm.py, and other files

--- diff.sh
+++
@@ -1,6 +0,0 @@
-# run to compare vm.c and vm.py
-
-./run.sh > a.txt
-python3 vm.py > b.txt
-echo "and compare..."
-diff a.txt b.txt
--- old_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename="array"
-
-# -O turns printing off
-python3 asm.py $filename.asm
-python3 vm.py  # -O
