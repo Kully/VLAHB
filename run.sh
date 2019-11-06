93d23288e7f6c9af9f52546d34f264c319aa976b
Kully <adam@plot.ly>
2019-11-06 12:55:36 -0500

    recolor pong to brown shades

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 asm.py conway.asm -s
+python3 asm.py pong.asm -s
 gcc -O3 -march=native -Wall -Wextra -Wpedantic bin.c && ./a.out hex/file.hex bin/file.bin
 gcc -O3 -march=native -Wall -Wextra -Wpedantic vm.c -lSDL2 -lm && ./a.out bin/file.bin
